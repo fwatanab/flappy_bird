7f8dd327b28aed785cb2dc4d9d85c9647004d7fc
fwatanab <fwatanab@student.42tokyo.jp>
2025-08-27 14:00:53 +0900

    パイプを管理するオブジェクトの実装

--- src/objects/column_manager.py
+++ src/objects/column_manager.py
@@ -1,29 +1,37 @@
-import pygame
+from kivy.core.window import Window
+from kivy.uix.widget import Widget
 from objects.column import Column
-from config import PIPE_SPAWN_INTERVAL, SCREEN_WIDTH
+from config import PIPE_SPAWN_INTERVAL
 
-class Column_manager:
-	def __init__(self, image):
-		self.image = image
+class ColumnManager(Widget):
+	def __init__(self, texture, **kwargs):
+		super(ColumnManager, self).__init__(**kwargs)
+		self.texture = texture
 		self.columns = []
-		self.spawn_timer = 0 # 次のパイプ生成までのカウント
+		self.spawn_timer = 0  # 次のパイプ生成までのカウント
+
+	def resize(self, *args):
+		for column in self.columns:
+			column.resize()
 
 	def update(self, dt):
 		# スポーンタイマーを更新
 		self.spawn_timer += dt
 		if self.spawn_timer >= PIPE_SPAWN_INTERVAL:
-			# パイプを生成し、リストに追加
-			new_column = Column(self.image, SCREEN_WIDTH + 20)
+			# 新しいパイプを生成してリストに追加
+			new_column = Column(self.texture)
+			self.add_widget(new_column)
 			self.columns.append(new_column)
-			self.spawn_timer = 0 # タイマーをリセット
+			self.spawn_timer = 0  # タイマーをリセット
 
-		# 各パイプの更新とスクロール
+		# 各パイプを更新
 		for column in self.columns:
-			column.update()
+			column.update(dt)
 
 		# 画面外に出たパイプを削除
-		self.columns = [col for col in self.columns if col.top_rect.right > 0]
-	
-	def draw(self, screen):
-		for column in self.columns:
-			column.draw(screen)
+		self.columns = [col for col in self.columns if not col.is_out_of_screen()]
+
+		# 不要なウィジェットも削除
+		for col in self.children[:]:
+			if col not in self.columns:
+				self.remove_widget(col)
